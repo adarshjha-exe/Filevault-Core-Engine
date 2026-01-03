ba411b0dcaccc63c229e4d7049c496bec8a40b1c
Adarsh <jha01adarsh@gmail.com>
2026-01-03 20:48:44 +0530

    display all file name

--- app.js
+++ app.js
@@ -5,7 +5,26 @@ import http from 'node:http';
 const server = http.createServer(async (req, res) => {
   // get all files/folder from the directory
   const filesAndFolderItems = await readdir('./storage', { recursive: true });
-  res.end(`Directory items : ${filesAndFolderItems}`);
+
+  //dynamic HTML
+  let dynamicHTML = '';
+  filesAndFolderItems.forEach((file) => {
+    dynamicHTML += `<li>${file} </li>`;
+  });
+
+  // server response
+  res.end(`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>Your Files :</h1>
+    <ul> ${dynamicHTML} </ul>
+  </body>
+</html>`);
 });
 
 server.listen(3000, '0.0.0.0', () => {
